65c504bcf536c390f2992dfe72d0c6b5a306e5ce
roksana <test@test.com>
2023-11-07 22:17:28 -0500

    test code

---
+++ src/main/java/com/bush_practice/bush_practice/App.java
@@ -0,0 +1,13 @@
+package com.bush_practice.bush_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
